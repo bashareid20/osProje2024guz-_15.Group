b487a1a60ee8fc591aa1d447b19a55a80d57cc13
abdullah-mulle1 <133582938+abdullah-mulle1@users.noreply.github.com>
2024-12-27 14:51:44 +0300

    Add files via upload

---
+++ girisyonlendirme.c
@@ -0,0 +1,46 @@
+void execute_command(char **args) {
+    pid_t pid;
+    int in_redirect_index = -1;
+
+    // Giriş yönlendirme sembolünü bul
+    for (int i = 0; args[i] != NULL; i++) {
+        if (strcmp(args[i], "<") == 0) {
+            in_redirect_index = i;
+            break;
+        }
+    }
+
+    if (in_redirect_index != -1) {
+        if (args[in_redirect_index + 1] == NULL) {
+            fprintf(stderr, "Giriş dosyası belirtilmedi.\n");
+            return;
+        }
+    }
+
+    pid = fork();
+    if (pid < 0) {
+        perror("Fork hatası");
+    } else if (pid == 0) {
+        // Çocuk işlem
+        if (in_redirect_index != -1) {
+            FILE *input_file = fopen(args[in_redirect_index + 1], "r");
+            if (!input_file) {
+                perror("Giriş dosyası açılamadı");
+                exit(EXIT_FAILURE);
+            }
+            dup2(fileno(input_file), STDIN_FILENO);
+            fclose(input_file);
+
+            // Yönlendirme işaretçisini ve dosya adını kaldır
+            args[in_redirect_index] = NULL;
+        }
+
+        if (execvp(args[0], args) < 0) {
+            perror("Komut çalıştırılamadı");
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        // Ana işlem
+        wait(NULL);
+    }
+}
